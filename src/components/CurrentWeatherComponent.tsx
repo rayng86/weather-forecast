afe0896407b9a49e3375ecbd1fce59c5aebac21d
Raymond Ng <rayng86@gmail.com>
2021-01-03 10:48:41 -0500

    toggle between fahrenheit and celcius working

--- src/components/CurrentWeatherComponent.tsx
+++ src/components/CurrentWeatherComponent.tsx
@@ -1,9 +1,8 @@
 import React from 'react';
-import { degreeTextSymbol } from '../constants';
-import { WeatherDataProps } from '../types';
-import { useNightTimeCloudIcon } from '../utils';
+import { CurrentWeatherComponentProps } from '../types';
+import { calculateByMeasurementType, useNightTimeCloudIcon } from '../utils';
 
-const CurrentWeatherComponent = ({ weatherData } : WeatherDataProps) => {
+const CurrentWeatherComponent = ({ weatherData, measurementType } : CurrentWeatherComponentProps) => {
   const { temp: currentTemp, feels_like: feelsLike, temp_min: tempMin, temp_max: tempMax } = weatherData.data2.main;
   const weatherIcon = weatherData.data2.weather[0].id;
   const { name: locationName } = weatherData.data2;
@@ -15,13 +14,13 @@ const CurrentWeatherComponent = ({ weatherData } : WeatherDataProps) => {
         <h3>{weatherData.data2.weather[0].description}</h3>
         <hr />
         <h5>Current Temp.</h5>
-        <p>{currentTemp}{degreeTextSymbol}</p>
+        <p>{calculateByMeasurementType(measurementType, currentTemp)}</p>
         <h5>Feels Like</h5>
-        <p>{feelsLike}{degreeTextSymbol}</p>
+        <p>{calculateByMeasurementType(measurementType, feelsLike)}</p>
         <h5>Temp. Low</h5>
-        <p>{tempMin}{degreeTextSymbol}</p>
+        <p>{calculateByMeasurementType(measurementType, tempMin)}</p>
         <h5>Temp. High</h5>
-        <p>{tempMax}{degreeTextSymbol}</p>
+        <p>{calculateByMeasurementType(measurementType, tempMax)}</p>
       </figcaption>
       <figcaption>
         <p><small>Created by Raymond Ng</small></p>
